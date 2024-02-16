e639eb4ac4b3ba763e6925e9e8cc018931528cba
AR-DThon <160032870+AR-DThon@users.noreply.github.com>
2024-02-16 22:49:04 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -13,13 +13,12 @@ class Development(Config):
     STRING_SESSION = "Your value"
     # create a new bot in @botfather and fill the following vales with bottoken
     TG_BOT_TOKEN = "Your value"
+    TG_BOT_USERNAME = "Your value"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
     PRIVATE_GROUP_BOT_API_ID = -100
     # command handler
     COMMAND_HAND_LER = "."
+    # sudo enter the id of sudo users userid's in that array
+    SUDO_USERS = []
     # command hanler for sudo
     SUDO_COMMAND_HAND_LER = "."
-    # External plugins repo
-    EXTERNAL_REPO = "https://github.com/TgCatUB/CatPlugins"
-    # if you need badcat plugins set "True"
-    BADCAT = "False"
